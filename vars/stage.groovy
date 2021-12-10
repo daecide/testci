d5e9beebfd30c0dc5c1b4dfbe8a1e09f86195526
Ferry Sutanto <nakama@Ferrys-MacBook-Pro.local>
2021-12-10 16:22:23 +0700

    debug

--- vars/stage.groovy
+++ vars/stage.groovy
@@ -1,7 +1,7 @@
 def call(String name, Closure closure) {
     echo "HELLO"
     try {
-        this.stage(name, closure)
+        return this.stage(name, closure)
     } finally {
         echo "WORLD"
     }
